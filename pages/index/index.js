c95b695295da19c9ea1c9ea839ca7294ca62db60
naceyeve <271948021@qq.com>
2020-01-11 22:32:59 +0800

    first commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,114 @@
 
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
 
 Page({
-  onLoad(){
 
+  data:{
+    weather:'',
+    temp:'',
+    weatherBackground:'',
+    hourlyWeather: [],
+    todayTime:'',
+    todayTemp:''
+  },
+  onPullDownRefresh(){
+    this.getData(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  getData(callBack){
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now?city=%E4%B8%8A%E6%B5%B7%E5%B8%82',
-      data:{
-        city:'上海市'
+      data: {
+        city: '上海市'
       },
       header: {
         'content-type': 'application/json' // 默认值
       },
-      success:res => {
+      success: res => {
         console.log(res.data)
+        let result = res.data.result
+        this.getNowData(result)
+        this.getHourlyData(result)
+        this.setToday(result)
+
+      },
+      complete: ()=>{
+        callBack && callBack()
+    
       }
+
+    
     })
+  },
+  getNowData(result){
+    let weahter1 = result.now.weather
+    let temp2 = result.now.temp
+    console.log(weahter1, temp2)
+    this.setData({
+      weather: weatherMap[weahter1],
+      temp: temp2 + '*',
+      weatherBackground: '/images/' + weahter1 + '-bg.png'
+    })
+
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weahter1],
+    })
+  },
+  getHourlyData(result){
+    let forecast = result.forecast
+    let hourlyWeather = []
+    let hours = new Date().getHours()
+    for (let i = 0; i < 8; i++) {
+      hourlyWeather.push({
+        time: (i * 3 + hours) % 24 + "时",
+        path: "/images/" + forecast[i].weather + "-icon.png",
+        temp: forecast[i].temp
+
+      })
+    }
+    hourlyWeather[0].time = "现在"
+    this.setData({ hourlyWeather: hourlyWeather })
+  },
+  setToday(result){
+    let date =  new Date();
+    this.setData({
+      todayTime: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 今天`,
+      todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`
+    })
+
+  },
+  toTodayDetail(){
+      wx.navigateTo({
+        url: '/pages/list/list'
+      })
+  },
+  getLocation(){
+     wx.getLocation({
+       success: res => {
+         console.log(res.latitude,res.longitude)
+       },
+     })
+  },
+  onLoad(){
+    this.getData()
+    
   }
 })
